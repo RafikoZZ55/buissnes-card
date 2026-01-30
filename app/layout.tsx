b93243814521067ec69824e0a1ddcb2c2c6a7997
RafikoZZ55 <rafalek.maseraczek@gmail.com>
2026-01-30 16:12:43 +0100

    started working on buissner-card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Jersey_10 } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const jersey_10 = Jersey_10({
+  variable: "--font-jersey-10",
+  weight: "400",
+  style: "normal",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jersey_10.variable} antialiased`}
       >
         {children}
       </body>
